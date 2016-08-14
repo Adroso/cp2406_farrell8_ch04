e9b0ac85860923f4394473eb2d7f2163444e88a2
Adroso <adrian.lapico@my.jcu.edu.au>
2016-08-15 00:27:12 +1000

    Excersize 1

---
+++ Excersises/src/FormLetterWriter.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Adroso360 on 15/08/2016.
+ */
+import java.util.Scanner;
+public class FormLetterWriter {
+    public static void main(String[] args) {
+        String fname =  getName("Enter your First Name");
+        String lname =  getName("Enter your Last Name");
+        displaySalutation(lname);
+        displaySalutation(lname, fname);
+
+    }
+
+    private static String displaySalutation(String cusLastname){
+
+        System.out.println("Dear Mr. or Ms." + cusLastname + " Thank you for giving us your money.");
+        return "Dear Mr. or Ms. " + cusLastname;
+    }
+
+    private static String displaySalutation(String cusLastname, String cusFirstname){
+
+        System.out.println("Dear." + cusFirstname + " " + cusLastname + " Thank you for giving us your money.");
+        return "Dear Mr. or Ms. " + cusLastname;
+    }
+
+    private static String getName(String message){
+        Scanner reader = new Scanner(System.in);
+        System.out.println(message);
+        String n = reader.next();
+        return n;
+    }
+}
